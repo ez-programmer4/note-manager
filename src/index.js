6428b933802f2d4850d6ea6f4c1351158c45df21
ezedin ebrahim <ezedinebrahim131@gmail.com>
2024-11-15 11:25:41 +0300

    deploying1

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router } from "react-router-dom"; // Importing Router
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-if (ProcessingInstruction.env.NODE_ENV === "production") disableReactDevTools();
+
+// Use process.env to access NODE_ENV
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const container = document.getElementById("root");
 const root = createRoot(container);
@@ -12,8 +14,6 @@ const root = createRoot(container);
 root.render(
   <ChakraProvider>
     <Router>
-      {" "}
-      {/* Only one Router wrapping the whole app */}
       <App />
     </Router>
   </ChakraProvider>
